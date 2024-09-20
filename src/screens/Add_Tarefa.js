c1c4c65330918e9568c46d23c1a1399afb0ca17a
unknown <vilelacostamc@gmail.com>
2024-09-20 10:04:37 -0300

    a

---
+++ src/screens/Add_Tarefa.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import {
+    Text,
+    TextInput,
+    View,
+    StyleSheet,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Modal
+} from "react-native"
+
+export default class Add_Tarefa extends Component{
+    render(){
+        return(
+            
+        )
+    }
+}
